e914dc238e0f47d3f0dd5d102f95bba9461cffe0
Glitch (tjhickey724-highschoolappdesign-1) <none>
2021-06-29 18:54:43 -0400

    index auth

--- app.js
+++ app.js
@@ -118,9 +118,6 @@ app.post("/getPlayer",   async (req,res,next) => { 
 
 
 
-
-
-
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
